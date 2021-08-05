1b6677242ce23c20869886e688101ac731fc09cb
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-08-05 15:27:24 +0100

    polling unit page

--- results_app/urls.py
+++ results_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index, get_lga
+from .views import index, get_lga, get_pu
 
 urlpatterns = [
     path('', index, name="index"),
-    path('lga/<str:pk>', get_lga, name="get_lga")
-
+    path('lga/<str:pk>', get_lga, name="get_lga"),
+    path('pu-results/<str:pk>', get_pu, name="polling_unit")
 ]
--- results_app/views.py
+++ results_app/views.py
@@ -16,12 +16,24 @@ def index(request):
 
 def get_lga(request, pk):
     cursor = connection.cursor()
-    results = "SELECT party_abbreviation, party_score FROM announced_lga_results WHERE lga_name = "+pk+""
-    # local_gvt = "SELECT lga_name FROM lga WHERE lga_id = "+pk+""
+    data = "SELECT polling_unit_id, polling_unit_name FROM polling_unit WHERE lga_id = "+pk+""
 
-    # cursor.execute(local_gvt)
-    cursor.execute(results)
-    results = cursor.fetchall()
-    context = {"result": results}
-    print(results)
+    first_data = cursor.execute(data)
+    results = first_data.fetchall()
+
+    total_votes = "SELECT party_abbreviation, party_score FROM announced_lga_results WHERE lga_name = "+pk+""
+    second_data = cursor.execute(total_votes)
+    second_result = second_data.fetchall()
+    
+    context = {"result": results, "votes": second_result}
     return render(request, 'results_app/lga_results.html', context)
+
+
+def get_pu(request, pk):
+    cursor = connection.cursor()
+    data = "SELECT party_abbreviation, party_score FROM announced_pu_results WHERE polling_unit_uniqueid = "+pk+""
+    cursor.execute(data)
+    result = cursor.fetchall()
+    context = {"result": result}
+    print(result)
+    return render(request, 'results_app/pu_results.html', context)
